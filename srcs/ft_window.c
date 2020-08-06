2a9cac9d97430fb6e07691863e347b746b30a7f8
limartin <lindsayjamesmartin94@gmail.com>
2020-08-06 17:53:28 +0200

    6 August 2020, And on the sixth day of His holiday He split the heavens and the earth, and Lo, He saw it was good (more specifically: the ceiling is now ceiling-coloured and the floor is floor-coloured, after deciding to do some coding today). But He also saw that it only shows a colour on the screen about half the time, and half the time the screen just stays black. He has a suspicion this might sort itself if he implements the mlx loop hooking, and if he stops referring to himself in the third person. Also quickly wrote the function that makes the window resize if the resolution is too large.

--- srcs/ft_window.c
+++ srcs/ft_window.c
@@ -6,11 +6,15 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/23 15:39:24 by lindsay       #+#    #+#                 */
-/*   Updated: 2020/07/29 14:35:48 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/08/06 17:46:19 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cub3d.h"
+
+void	ft_heavensplit(t_mapinfo *m, t_img imga);
+
+void	ft_resize(void *mlx, t_mapinfo *m);
 
 int		ft_window(t_mapinfo *m)
 {
@@ -21,16 +25,49 @@ int		ft_window(t_mapinfo *m)
 	mlx = mlx_init();
 	if (mlx == 0)
 		xt_mlxerror(m);
+	ft_resize(mlx, m);
 	mlx_win = mlx_new_window(mlx, m->resx, m->resy, "Test");
 	if (mlx_win == 0)
 		xt_mlxerror(m);
 	imga.cont = mlx_new_image(mlx, m->resx, m->resy);
 	imga.addr = mlx_get_data_addr(imga.cont, &imga.bits_per_pixel, \
 	&imga.line_bytes, &imga.endian);
+	ft_heavensplit(m, imga);
+	mlx_put_image_to_window(mlx, mlx_win, imga.cont, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
 
+void	ft_resize(void *mlx, t_mapinfo *m)
+{
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+	mlx_get_screen_size(mlx, &x, &y);
+	if (x < m->resx)
+		m->resx = x;
+	if (y < m->resy)
+		m->resy = y;
+}
+
+void	ft_put_pixel_img(t_img *img, int x, int y, int colour)
+{
+	char	*dst;
+	int		pxl_mem_size;
+
+	pxl_mem_size = (img->bits_per_pixel / 8);
+	dst = img->addr + (y * img->line_bytes + x * pxl_mem_size);
+	*(unsigned int*)dst = colour;
+}
+
+void	ft_heavensplit(t_mapinfo *m, t_img imga)
+{
 	int		x;
 	int		y;
 	int		colour;
+
 	colour = ((m->cb) + (m->cg * 16 * 16) + (m->cr * 16 * 16 * 16 * 16));
 	y = 0;
 	while (y < m->resy)
@@ -42,18 +79,7 @@ int		ft_window(t_mapinfo *m)
 			x++;
 		}
 		y++;
+		if (y >= (m->resy / 2))
+			colour = ((m->fb) + (m->fg * 256) + (m->fr * 256 * 256));
 	}
-	mlx_put_image_to_window(mlx, mlx_win, imga.cont, 0, 0);
-	mlx_loop(mlx);
-	return (0);
-}
-
-void	ft_put_pixel_img(t_img *img, int x, int y, int colour)
-{
-	char	*dst;
-	int		pxl_mem_size;
-
-	pxl_mem_size = (img->bits_per_pixel / 8);
-	dst = img->addr + (y * img->line_bytes + x * pxl_mem_size);
-	*(unsigned int*)dst = colour;
 }
