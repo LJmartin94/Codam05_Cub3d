4ec23f64aa20bad44e01b6e91e4afa2a340cd3f7
limartin <lindsayjamesmartin94@gmail.com>
2020-09-17 17:24:17 +0200

    17 September 2020, Nevermind! I just wrote an actual bubble sort algorithm, and the bidirectional one is faster (measured in iterations of swaps and/or checks), which is a nice surprise! For things in exactly the wrong order it doesn't make a difference, but on a jumbling up of letters the bidirectional bubble sort was able to sort it in 1024 iterations where the regular bubble sort did it in 1322. And I still believed the advantage it has can be leveraged further to make data sets less prone to being held hostage to outlying data. And another good feature it has is that if either algorithm is unable to finish their computation, the in-between product of the bidirectional bubble sort will contain far less stark differences between adjacent members of the set, as you would expect to be the product of both sorting methods upon completion, whilst it is also almost instantly able to retrieve a sets highest and lowest value even when the computation is interrupted. Perhaps today was not a waste of time after all.

--- srcs/ft_sort.c
+++ srcs/ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/17 13:31:23 by lindsay       #+#    #+#                 */
-/*   Updated: 2020/09/17 16:47:46 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/09/17 17:11:51 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,57 @@ int	ft_bibubblesort(char **tosort)
 	return (items);
 }
 
+int	ft_actualbubblesort(char **tosort)
+{
+	int items;
+	int i;
+	int done;
+	int holdvalue;
+	int counter;
+	int change;
+	int schg;
+
+	items = 0;
+	while ((*tosort)[items] != '\0')
+		items++;
+	printf("items: %d\n", items);
+	i = 0;
+	done = 0;
+	counter = 0;
+	change = 0;
+	printf("%d)	string: %s\n", counter, (*tosort));
+	schg = items - 1;
+	while (!done)
+	{
+		if (i == schg)
+		{
+			schg--;
+			i = 0;
+		}
+		printf("	comparing: '%c'[%d] to '%c'[%d] next	(comparing values between %d and %d)\n", (*tosort)[i], i, (*tosort)[i + 1], i + 1, 0, schg);
+		if ((*tosort)[i] >= (*tosort)[i + 1])
+			i++;
+		else
+		{
+			holdvalue = (*tosort)[i];
+			(*tosort)[i] = (*tosort)[i + 1];
+			(*tosort)[i + 1] = holdvalue;
+			i = i + 1;
+			change = 0;
+		}
+		change++;
+		counter++;
+	//	if ((schg - lchg) < items / 2)
+		if (change > schg)
+			done = 1;
+		printf("%d)	string: %s", counter, (*tosort));
+	}
+	return (items);
+}
+
 int	main(int argc, char **argv)
 {
 	ft_bibubblesort(&argv[1]);
+	//ft_actualbubblesort(&argv[1]);
 	return (0);
 }
