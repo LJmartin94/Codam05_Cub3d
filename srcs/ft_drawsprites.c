9469ba337cb8cf7561cb1b43fa2837fac89d95c5
limartin <lindsayjamesmartin94@gmail.com>
2020-09-29 11:01:54 +0200

    29 September 2020, The function that initially fetches the sprites and stores them all in an array has now been written. Next step is sorting them.

--- srcs/ft_drawsprites.c
+++ srcs/ft_drawsprites.c
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/28 16:49:27 by lindsay       #+#    #+#                 */
-/*   Updated: 2020/09/28 17:59:20 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/09/29 10:58:33 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 int		ft_scanforsprites(t_data *d)
 {
+	int y;
+	int x;
+	int s;
+
+	y = 0;
+	s = 0;
+	while (d->m->map[y] != NULL)
+	{
+		x = 0;
+		while (d->m->map[y][x] != '\0')
+		{
+			if (d->m->map[y][x] == '2' && d->m->copy->map[y][x] == 'x')
+			{
+				d->m->sprites[s].y = y;
+				d->m->sprites[s].x = x;
+				s++;
+			}
+			x++;
+		}
+		y++;
+	}
 	return (0);
 }
 
